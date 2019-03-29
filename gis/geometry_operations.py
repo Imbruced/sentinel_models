bf0b3e1a9979d93c372967971f0caadce8344085
Pawel <pawel93kocinski@gmail.com>
2019-03-30 00:23:44 +0100

    added count delta functionn documentation and types from typing library

--- gis/geometry_operations.py
+++ gis/geometry_operations.py
@@ -1,11 +1,12 @@
+from typing import Union
 
 
-
-def count_delta(start, end):
+def count_delta(start: Union[int, float], end: Union[int, float]) -> Union[int, float]:
     """
     Count the difference between two values
-    :param start:
-    :param end:
-    :return:
+    :param start: Union[int, float]
+    :param end: Union[int, float]
+    :return: Union[int, float]
     """
+
     return end - start
