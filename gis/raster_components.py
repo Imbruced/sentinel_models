169d80afa55cb16103d85d9f803954633e6f4c20
Pawel <pawel93kocinski@gmail.com>
2019-03-31 00:18:39 +0100

    Moved Standarizer into other module, Simplified class methods from_geo and from wkt

--- gis/raster_components.py
+++ gis/raster_components.py
@@ -1,7 +1,6 @@
 import attr
 from gis.log_lib import logger
 from gis.validators import ispositive
-from config.config import Config
 from gis.geometry import Point
 from gis.geometry import Origin
 import numpy as np
@@ -10,107 +9,10 @@ import os
 from osgeo import osr
 import ogr
 from gis.geometry import GeometryFrame
-import matplotlib.pyplot as plt
 import json
 from typing import List
-
-
-class StanarizeParams:
-
-    _band_number = attr.ib()
-    _coefficients = {}
-
-    def __init__(self, band_number):
-        self._band_number = band_number
-
-    def add(self, coeff, band_name):
-        if len(self._coefficients.keys()) >= self._band_number:
-            raise OverflowError("Value is to high")
-        else:
-            try:
-                self._coefficients.get(band_name)
-            except KeyError:
-                self._coefficients[band_name] = coeff
-
-    @property
-    def coefficients(self):
-        return self._coefficients
-
-    @coefficients.setter
-    def coefficients(self, value):
-        raise ValueError("Value can not be set, please use add method")
-
-
-class ImageStand:
-    """
-    Class is responsible for scaling image data, it requires gis.raster.Raster object
-    Band names can be passed, by default is ["band_index" ... "band_index+n"]
-    use standarize_image method to standarize data, it will return Raster with proper refactored image values
-
-    """
-
-    def __init__(self, raster, length, names=None):
-        self.__stan_params = StanarizeParams(length)
-        self.__names = [f"band_{band}" for band in range(length)] if names is None else names
-        logger.info(self.__names)
-        self.__raster = raster
-
-    def save_params(self, path):
-        if os.path.exists(path):
-            raise FileExistsError("File with this name exists in directory")
-        with open(path, "w") as file:
-            params_json = json.load(self.__stan_params.coefficients)
-            file.writelines(params_json)
-
-    def standarize_image(self, rescale_function=None):
-        """
-        Rescale data by maximum
-        :return:
-        """
-        empty_array = np.empty(shape=[*self.__raster.ref.shape, self.__raster.ref.band_number])
-
-        for index, (band, name) in enumerate(zip(self.__raster.array, self.__names)):
-            empty_array[:, :, index] = self.__stand_one_dim(band, name, rescale_function)
-            band = None
-
-        ref = ReferencedArray(empty_array, self.__raster.ref.extent, self.__raster.pixel, shape=self.__raster.ref.shape, band_number=self.__raster.ref.band_number)
-
-        return Raster(self.__raster.pixel, ref)
-
-    def __stand_one_dim(self, array: np.ndarray, band_name: str, rescale_function=None):
-        """
-        Standarizing 2 dim array, fnct for rescaling can be passed: require function which takes array and returns
-        array
-        :param array:
-        :return:
-        """
-        if rescale_function is None:
-            stand_value = self.find_max(array)
-        else:
-            stand_value = rescale_function(array)
-
-        self.__stan_params.add(stand_value, band_name)
-        return array/stand_value
-
-    def find_max(self, array):
-        return 7000
-        # return array.max()
-
-    @property
-    def stan_params(self):
-        return self.__stan_params
-
-    @stan_params.setter
-    def stan_params(self, value):
-        raise AttributeError("This param can' t be set")
-
-
-class ConfigMeta(type):
-
-    def __new__(mcs, *args, **kwargs):
-        x = super().__new__(mcs, *args, **kwargs)
-        x.config = Config.from_json(x.__name__.lower())
-        return x
+from gis.geometry import Extent
+from gis.meta import ConfigMeta
 
 
 @attr.s
@@ -128,21 +30,6 @@ class Pixel(metaclass=ConfigMeta):
 @attr.s
 class Crs(metaclass=ConfigMeta):
     epsg = attr.ib(default="epsg:4326", validator=[attr.validators.instance_of(str)])
-
-
-@attr.s
-class Extent:
-
-    left_down = attr.ib(default=Point(0, 0))
-    right_up = attr.ib(default=Point(1, 1))
-    origin = attr.ib(init=False)
-    dx = attr.ib(init=False)
-    dy = attr.ib(init=False)
-
-    def __attrs_post_init__(self):
-        self.origin = Origin(self.left_down.x, self.left_down.y)
-        self.dx = self.right_up.dx(self.left_down)
-        self.dy = self.right_up.dy(self.left_down)
 
 
 @attr.s
@@ -195,11 +82,6 @@ class Raster:
         copy_raster = raster
         copy_raster.SetGeoTransform((origin.x, pixel.x, 0.0, origin.y+(8838*10), 0, -pixel.y))
         left_top_corner_x, pixel_size_x, _, left_top_corner_y, _, pixel_size_y = copy_raster.GetGeoTransform()
-        logger.info(left_top_corner_x)
-        logger.info(pixel_size_x)
-        logger.info(left_top_corner_y)
-        logger.info(pixel_size_y)
-
         copy_raster.SetProjection('LOCAL_CS["arbitrary"]')
 
         return copy_raster
@@ -226,9 +108,7 @@ class Raster:
         :return:
         """
 
-        point_a = Point(0, 0)
-        point_b = Point(int(extent.dx / pixel.x), int(extent.dy / pixel.y))
-        new_extent = Extent(point_a, point_b)
+        new_extent = extent.scale(pixel.x, pixel.y)
         raster = cls.__create_raster(new_extent.dx, new_extent.dy)
         transformed_raster = cls.__transform(raster, extent.origin, pixel)
         polygon_within = cls.__insert_polygon(transformed_raster, wkt, 1)
@@ -263,7 +143,7 @@ class Raster:
     def from_geo(cls, geoframe: GeometryFrame, extent: Extent, pixel: Pixel):
 
         """
-        TODO simplify this method
+        TODO simplify this method and looka at extent new object
         :param geoframe:
         :param extent:
         :param pixel:
@@ -272,9 +152,7 @@ class Raster:
         wkt_frame = geoframe.to_wkt()
         wkt_string = wkt_frame.frame["wkt"].values.tolist()[0]
 
-        point_a = Point(0, 0)
-        point_b = Point(int(extent.dx / pixel.x), int(extent.dy / pixel.y))
-        new_extent = Extent(point_a, point_b)
+        new_extent = extent.scale(pixel.x, pixel.y)
         raster = cls.__create_raster(new_extent.dx, new_extent.dy)
         transformed_raster = cls.__transform(raster, extent.origin, pixel)
 
@@ -283,6 +161,8 @@ class Raster:
         extent_new = Extent(Point(extent.origin.x, extent.origin.y),
                             Point((new_extent.origin.x + new_extent.dx) * pixel.x,
                                   (new_extent.origin.y + new_extent.dy) * pixel.y))
+        logger.info(extent_new)
+
         array = polygon_within.ReadAsArray()
 
         reshaped_array = array.reshape(*array.shape, 1)
@@ -324,26 +204,6 @@ class Raster:
             yield ds.GetRasterBand(band+1).ReadAsArray()
 
 
-class ImagePlot:
-
-    def __init__(self):
-        self._images = []
-
-    def plot(self):
-        figure, axes = plt.subplots(nrows=int(self._images.__len__()), ncols=2, figsize=(self._images.__len__()*5, 10), sharey =True)
-        for index, image in enumerate(self._images):
-            logger.info(index)
-            axes[index][0].imshow(image[0])
-            axes[index][1].imshow(image[1])
-        #     try:
-        #         axes[index][0].imshow(image[0])
-        #         axes[index][1].imshow(image[1])
-        #     except TypeError:
-        #         axes.imshow(image)
-        plt.show()
-
-    def add(self, array):
-        self._images.append(array)
 
 
 @attr.s
@@ -357,8 +217,6 @@ class ArrayShape:
     def __ne__(self, other):
         return self.x_shape != other.x_shape or self.y_shape != other.y_shape
 
-    def __eq__(self, other):
-        return self.x_shape == other.x_shape or self.y_shape == other.y_shape
 
 class RasterData:
     """
