8e5198391c6df722aa97a5f872bd7f85c0a46c9e
Pawel <pawel93kocinski@gmail.com>
2019-04-03 23:41:28 +0200

    Created class for handling Coordinate reference system exception.

--- gis/exceptions.py
+++ gis/exceptions.py
@@ -15,3 +15,9 @@ class LessThanZeroException(Exception):
 
     def __init__(self, message):
         self.message = message
+
+
+class CrsException(Exception):
+
+    def __init__(self, message):
+        super().__init__(message)
