64fcc276b34e2d63c619bb4e407fec53d73e28e1
Pawel <pawel93kocinski@gmail.com>
2019-04-04 00:03:29 +0200

    Updated one of usage example

--- tests/model.py
+++ tests/model.py
@@ -7,9 +7,9 @@ import matplotlib.pyplot as plt
 matplotlib.use('Qt5Agg')
 
 if __name__ == "__main__":
-    shape_path = "~tests\\domy.shp"
+    shape_path = "D:\\master_thesis\\PYTHON\\sentinel_models\\tests\\domy.shp"
     polygon_frame = PolygonFrame.from_file(shape_path)
-    main_image = Raster.from_file("~tests\\buildings.tif")
+    main_image = Raster.from_file("D:\\master_thesis\\PYTHON\\sentinel_models\\tests\\buildings.tif")
     label_raster = Raster.with_adjustment("from_geo", main_image, polygon_frame)
 
     plt.imshow(label_raster.array[:, :, 0])
