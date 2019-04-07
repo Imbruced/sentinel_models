ac27cc6beaace89cec48e70280d4d78c5c34a614
Pawel <pawel93kocinski@gmail.com>
2019-04-07 21:49:13 +0200

    Added pixel size exception.

--- gis/exceptions.py
+++ gis/exceptions.py
@@ -21,3 +21,9 @@ class CrsException(Exception):
 
     def __init__(self, message):
         super().__init__(message)
+
+
+class PixelSizeException(Exception):
+
+    def __init__(self, message):
+        super().__init__(message)
