c5c349388337035373b11f99c31bc33754beeb04
Pawel <pawel93kocinski@gmail.com>
2019-05-02 22:35:34 +0200

    validators importing fix

--- gis/image_loader.py
+++ gis/image_loader.py
@@ -1,6 +1,8 @@
 import gdal
 import attr
-from gis.validators import IsImageFile
+import numpy as np
+
+from validators.validators import IsImageFile
 from gis.raster_components import Pixel
 from gis.geometry import Extent
 
@@ -41,7 +43,7 @@ class GdalImage:
         self.band_number = self.ds.RasterCount
 
     @classmethod
-    def load_from_file(cls, path, crs):
+    def load_from_file(cls, path: str, crs: str):
         """
         class method which based on path returns GdalImage object,
         It validates path location and its format
@@ -52,7 +54,7 @@ class GdalImage:
 
         return cls(ds, path, crs)
 
-    def __read_as_array(self, ds: gdal.Dataset):
+    def __read_as_array(self, ds: gdal.Dataset) -> np.ndarray:
         if not hasattr(self, "__array"):
             setattr(self, "__array", ds.ReadAsArray())
         return getattr(self, "__array")
@@ -61,12 +63,15 @@ class GdalImage:
         return "\n".join([f"{key}: {value}" for key, value in self.__dict__.items()])
 
     @property
-    def array(self):
+    def array(self) -> np.ndarray:
         """
         Property which returns numpy.ndarray representation of file
         :return: np.ndarray
         """
         return self.__read_as_array(self.ds)
+
+    def save(self, path):
+        pass
 
 
 
