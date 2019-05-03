b190e28f16f80a8244dcc9615860539905f0402c
Pawel <pawel93kocinski@gmail.com>
2019-05-03 21:44:05 +0200

    plottting method fix, added extend method

--- plotting/plotting.py
+++ plotting/plotting.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from logs.log_lib import logger
 import matplotlib.pyplot as plt
 
@@ -7,17 +9,19 @@ class ImagePlot:
     def __init__(self):
         self._images = []
 
-    def plot(self):
-        figure, axes = plt.subplots(nrows=int(self._images.__len__()),
-                                    ncols=2,
+    def plot(self, nrows=1):
+        figure, axes = plt.subplots(nrows=nrows,
+                                    ncols=int(self._images.__len__()),
                                     figsize=(self._images.__len__()*5, 10),
-                                    sharey="true")
+                                    sharey="all")
 
         for index, image in enumerate(self._images):
             logger.info(index)
-            axes[index][0].imshow(image[0])
-            axes[index][1].imshow(image[1])
+            axes[index].imshow(image)
         plt.show()
 
     def add(self, array):
         self._images.append(array)
+
+    def extend(self, images: Union[list, tuple]):
+        self._images.extend(list(images))
