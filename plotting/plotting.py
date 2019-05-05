4cec445f4e1571da3bf634820d6d311f34b3b019
Pawel <pawel93kocinski@gmail.com>
2019-05-05 23:05:19 +0200

    Number of cols based of number of rows

--- plotting/plotting.py
+++ plotting/plotting.py
@@ -10,14 +10,21 @@ class ImagePlot:
         self._images = []
 
     def plot(self, nrows=1):
+        image_numbers = self._images.__len__()
+        ncols = image_numbers // nrows
+        logger.info(f"Cols number: {ncols}")
+        logger.info(f"Row number {nrows}")
+
         figure, axes = plt.subplots(nrows=nrows,
-                                    ncols=int(self._images.__len__()),
+                                    ncols=ncols,
                                     figsize=(self._images.__len__()*5, 10),
                                     sharey="all")
 
         for index, image in enumerate(self._images):
             logger.info(index)
-            axes[index].imshow(image)
+            curr_column = (index + ncols) % ncols
+            curr_row = index // ncols
+            axes[curr_row][curr_column].imshow(image)
         plt.show()
 
     def add(self, array):
