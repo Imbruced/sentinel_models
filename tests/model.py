7732a835dc4b4de7efeb95429973054f92a718f3
Pawel <pawel93kocinski@gmail.com>
2019-05-05 23:04:54 +0200

    initial push

--- tests/model.py
+++ tests/model.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 from gis.data_prep import RasterData
 from models import Unet
 from models import UnetConfig
+from keras.optimizers import SGD
 from pprint import pprint
 from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 from plotting import ImagePlot
@@ -26,32 +27,44 @@ if __name__ == "__main__":
     # standarize2 = ImageStand(raster=main_image)
     raster_data = RasterData(standarize1.standarize_image(stand_scaler), label_raster)
     unet_images = raster_data.prepare_unet_images(image_size=(64, 64))
-    # callbacks = [
-    #     EarlyStopping(patience=10, verbose=1),
-    #     ReduceLROnPlateau(factor=0.1, patience=10, min_lr=0.0001, verbose=1),
-    #     ModelCheckpoint('model_12_11_600.h5', verbose=1, save_best_only=True, save_weights_only=False)
-    # ]
+    callbacks = [
+        EarlyStopping(patience=100, verbose=1),
+        ReduceLROnPlateau(factor=0.1, patience=100, min_lr=0, verbose=1),
+        ModelCheckpoint('model_more_class_pixels.h5', verbose=1, save_best_only=True, save_weights_only=False)
+    ]
     config = UnetConfig(
         input_size=[64, 64, 3]
     )
-    # unet = Unet(config=config)
-    # unet.compile(metrics=["accuracy"])
-    # unet.fit(unet_images, callbacks=callbacks, epochs=60, batch_size=8)
-    model_path = "C:\\Users\\Pawel\\Desktop\\sentinel_models\\tests\\model_12_11_600.h5"
+    unet = Unet(config=config)
+    # unet.compile(metrics=["accuracy"], optimizer=SGD(lr=0.001))
+    # unet.fit(unet_images, callbacks=callbacks, epochs=400, batch_size=2)
+    model_path = "C:\\Users\\Pawel\\Desktop\\sentinel_models\\tests\\model_more_class_pixels.h5"
     unet = Unet.load_from_weight_file(model_path, config=config)
     # logger.info(unet.config.model)
     # logger.info(type(unet))
-    predicted = unet.predict(unet_images.x_test[:1, :, :, :], threshold=0.7)
+    predicted = unet.predict(unet_images.x_test[:10, :, :, :], threshold=0.6)
 
     image_plot = ImagePlot()
     image_plot.extend(
         [
-            unet_images.x_test[0, :, :, :3],
-            unet_images.y_test[0, :, :, 0],
-            predicted[0, :, :, 0]
+            unet_images.x_test[1, :, :, :3],
+            unet_images.y_test[1, :, :, 0],
+            predicted[1, :, :, 0],
+            unet_images.x_test[2, :, :, :3],
+            unet_images.y_test[2, :, :, 0],
+            predicted[2, :, :, 0],
+            unet_images.x_test[3, :, :, :3],
+            unet_images.y_test[3, :, :, 0],
+            predicted[3, :, :, 0],
+            unet_images.x_test[4, :, :, :3],
+            unet_images.y_test[4, :, :, 0],
+            predicted[4, :, :, 0],
+            unet_images.x_test[5, :, :, :3],
+            unet_images.y_test[5, :, :, 0],
+            predicted[5, :, :, 0]
         ]
     )
-    image_plot.plot()
+    image_plot.plot(nrows=5)
 
     # raster_data.create_ann_frame().to_csv("C:\\Users\\Pawel\\Desktop\\sentinel_models\\example.csv")
     # plt.imshow(raster_data.create_ann_frame().reshape(1677, 1673, 4)[:, :, :3])
