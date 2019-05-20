a95d6174d5f861daed025d9312f808a6f64c4c8e
Pawel <pawel93kocinski@gmail.com>
2019-05-20 23:27:14 +0200

    Show case with unet model from file loading wages. It shows predict with plotting.

--- tests/model.py
+++ tests/model.py
@@ -1,31 +1,34 @@
+import os
+
 import matplotlib
 from gis.geometry import PolygonFrame
 from gis.raster import Raster
-from logs.log_lib import logger
 from gis.standarizer import ImageStand
 from sklearn.preprocessing import StandardScaler
-import matplotlib.pyplot as plt
 from gis.data_prep import RasterData
+
 from models import Unet
 from models import UnetConfig
-from keras.optimizers import SGD
-from pprint import pprint
 from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 from plotting import ImagePlot
 
 matplotlib.use('Qt5Agg')
 
 if __name__ == "__main__":
-    shape_path = "D:\\master_thesis\\PYTHON\\sentinel_models\\tests\\domy.shp"
+
+    module_path = os.path.split(os.getcwd())[0]
+
+    shape_path = os.path.join(module_path, "data", "domy.shp")
+    image_path = os.path.join(module_path, "data", "buildings.tif")
+    compiled_model = os.path.join(module_path, "data", "model_more_class_pixels.h5")
+
     polygon_frame = PolygonFrame.from_file(shape_path).union("id")
-    logger.info(polygon_frame.crs)
-    main_image = Raster.from_file("D:\\master_thesis\\PYTHON\\sentinel_models\\tests\\buildings.tif", crs="epsg:26917")
+
+    main_image = Raster.from_file(image_path, crs="epsg:26917")
     label_raster = Raster.with_adjustment("from_geo", main_image, polygon_frame)
-    stand_scaler = StandardScaler()
-    # max_scaler = MaxScaler()
+
     standarize1 = ImageStand(raster=main_image)
-    # standarize2 = ImageStand(raster=main_image)
-    raster_data = RasterData(standarize1.standarize_image(stand_scaler), label_raster)
+    raster_data = RasterData(standarize1.standarize_image(StandardScaler()), label_raster)
     unet_images = raster_data.prepare_unet_images(image_size=(64, 64))
     callbacks = [
         EarlyStopping(patience=100, verbose=1),
@@ -33,16 +36,13 @@ if __name__ == "__main__":
         ModelCheckpoint('model_more_class_pixels.h5', verbose=1, save_best_only=True, save_weights_only=False)
     ]
     config = UnetConfig(
-        input_size=[64, 64, 3]
+        input_size=[64, 64, 3],
+        callbacks=callbacks
     )
+
     unet = Unet(config=config)
-    # unet.compile(metrics=["accuracy"], optimizer=SGD(lr=0.001))
-    # unet.fit(unet_images, callbacks=callbacks, epochs=400, batch_size=2)
-    model_path = "C:\\Users\\Pawel\\Desktop\\sentinel_models\\tests\\model_more_class_pixels.h5"
-    unet = Unet.load_from_weight_file(model_path, config=config)
-    # logger.info(unet.config.model)
-    # logger.info(type(unet))
-    predicted = unet.predict(unet_images.x_test[:10, :, :, :], threshold=0.6)
+    unet = Unet.load_from_weight_file(compiled_model, config=config)
+    predicted = unet.predict(x=unet_images.x_test[:10, :, :, :], threshold=0.6)
 
     image_plot = ImagePlot()
     image_plot.extend(
@@ -65,11 +65,3 @@ if __name__ == "__main__":
         ]
     )
     image_plot.plot(nrows=5)
-
-    # raster_data.create_ann_frame().to_csv("C:\\Users\\Pawel\\Desktop\\sentinel_models\\example.csv")
-    # plt.imshow(raster_data.create_ann_frame().reshape(1677, 1673, 4)[:, :, :3])
-    # plt.show()
-    # plt.imshow(main_image.array[:, :, :3])
-    # plt.show()
-    # raster_data.save_unet_images([128, 128], "C:\\Users\\Pawel\\Desktop\\sentinel_models\\tests")
-
