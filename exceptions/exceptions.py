9ebf96308cd55828ee164c4e5ec95dc5fa009bbf
Pawel <pawel93kocinski@gmail.com>
2019-06-18 23:36:23 +0200

    [Addition] FormatNotAvailable exception added for handling situation when not available format was passed

--- exceptions/exceptions.py
+++ exceptions/exceptions.py
@@ -44,3 +44,8 @@ class ConfigException(Exception):
 class OptionNotAvailableException(Exception):
     def __init__(self, message):
         super().__init__(message)
+
+
+class FormatNotAvailable(Exception):
+    def __init__(self, message):
+        super().__init__(message)
